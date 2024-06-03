888598cac80815e28f2d9b34e361bb1e1756921f
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-06-03 22:46:07 +0300

    combine everything in one place

---
+++ web/src/index.js
@@ -0,0 +1,34 @@
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+import { App, WebServer } from './routing/index.js'
+import { router as test_router } from './test_router.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const hostname = 'localhost'
+const port = 3000
+
+const web_routes = {
+    '/css': { '*': '../../css' },
+    '/fonts': { '*': '../../fonts' },
+    '/img': { '*': '../../img' },
+    '/js': { '*': '../../js' },
+    '/docs': {
+        '': '../dist/index.html',
+        '*': '../dist',
+    },
+    '/': '../../pages/index.html',
+    '/add_poem': '../../pages/add_poem/index.html',
+}
+
+const app = new App()
+
+app.add(test_router, 'API Router', '/api')
+
+app.add(new WebServer(__dirname, web_routes), 'Web Server')
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+})
