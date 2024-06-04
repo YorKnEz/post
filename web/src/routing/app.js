983becc3d4e279e28103ed9e0039264175991ef4
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-06-04 10:09:12 +0300

    fix app prefix bug

--- web/src/routing/app.js
+++ web/src/routing/app.js
@@ -1,17 +1,20 @@
 import { Server } from 'http'
 
 // The App class represents the main class used for creating a server
-// 
+//
 // An App allows to add custom Handlers to it, which, as the name implies, handle requests
 // The handlers are called in the order of addition and the handling of the request stopts at the // first handler that is capable of handling the request
 export default class App extends Server {
     constructor(prefix = '') {
         super(async (req, res) => {
-            for (const handler of this.handlers) {
+            if (req.url.startsWith(this.prefix)) {
                 req.url = req.url.replace(this.prefix, '')
-                const matched = await handler.handle(req, res)
-                if (matched) {
-                    return
+
+                for (const handler of this.handlers) {
+                    const matched = await handler.handle(req, res)
+                    if (matched) {
+                        return
+                    }
                 }
             }
 
