3863b0b53ab5d303fef651d5688b0787f4021d39
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-06-04 10:46:35 +0300

    allow body reading

--- web/src/routing/app.js
+++ web/src/routing/app.js
@@ -5,10 +5,17 @@ import { Server } from 'http'
 // An App allows to add custom Handlers to it, which, as the name implies, handle requests
 // The handlers are called in the order of addition and the handling of the request stopts at the // first handler that is capable of handling the request
 export default class App extends Server {
+    static methodsWithBody = ['post', 'put', 'patch']
+
     constructor(prefix = '') {
         super(async (req, res) => {
             if (req.url.startsWith(this.prefix)) {
                 req.url = req.url.replace(this.prefix, '')
+
+                // try adding body to request
+                if (App.methodsWithBody.includes(req.method.toLowerCase())) {
+                    req.body = await this.getRequestBody(req)
+                }
 
                 for (const handler of this.handlers) {
                     const matched = await handler.handle(req, res)
@@ -26,6 +33,22 @@ export default class App extends Server {
 
         this.prefix = prefix
         this.handlers = []
+    }
+
+    getRequestBody = async (req) => {
+        return new Promise((resolve, reject) => {
+            req.body = []
+            req.on('data', (chunk) => {
+                req.body.push(chunk)
+            })
+                .on('end', () => {
+                    resolve(Buffer.concat(req.body).toString())
+                    // at this point, `req.body` has the entire request body stored in it as a string
+                })
+                .on('error', (e) => {
+                    reject(e)
+                })
+        })
     }
 
     add = (handler) => {
