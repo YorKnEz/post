dd509e1e441a6e136f13d869977891920b39b23c
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-06-04 12:50:56 +0300

    add codes

--- web/src/codes.js
+++ web/src/codes.js
@@ -1,9 +1,13 @@
 // Error codes to use in API responses
 export const ErrorCodes = Object.freeze({
     USER_NOT_FOUND: 1001,
+    ALBUM_NOT_FOUND: 1002,
+    POEM_NOT_FOUND: 1003,
 })
 
 // Success codes to use in API responses
 export const SuccessCodes = Object.freeze({
     USER_DELETED: 1,
+    ALBUM_DELETED: 2,
+    POEM_DELETED: 3,
 })
