3a9f2b6e23042886cd390100983604b8d8fff7ff
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-06-04 09:22:02 +0300

    add success codes

--- web/src/codes.js
+++ web/src/codes.js
@@ -2,3 +2,8 @@
 export const ErrorCodes = Object.freeze({
     USER_NOT_FOUND: 1,
 })
+
+// Success codes to use in API responses
+export const SuccessCodes = Object.freeze({
+    USER_DELETED: 1,
+})
