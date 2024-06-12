32018326836f52fa39002ba5112a08c2b07395a1
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-06-12 21:35:35 +0300

    move front to a separate folder

--- front/js/pages/index.js
+++ front/js/pages/index.js
@@ -1,5 +1,8 @@
 import { Navbar } from '../components/nav.js'
 import { Search } from '../components/search.js'
+import env from '../env.js'
+
+console.log(env.TEST)
 
 window.navbar = new Navbar()
 window.searchBar = new Search()
