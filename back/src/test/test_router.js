963417fd294e56ef132bf39f3a42b86e66a07d82
Alex Mitreanu <alexmitreanu04@gmail.com>
2024-06-15 10:11:43 +0300

    add an example of the new router features

--- back/src/test/test_router.js
+++
@@ -1,7 +0,0 @@
-import { JSONResponse, Router } from '../../../lib/routing/index.js'
-
-export const router = new Router('Test Router', '/api/test')
-
-router.post('/', async (req, res) => {
-    return new JSONResponse(200, 'Hello World')
-})
